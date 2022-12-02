1a15d23890bb592c812592cf7147855b9207f9b6
Janjiran <jiranjan03@gmail.com>
2022-12-02 10:45:20 +0100

    :sparkles: day02 2/2 done

--- 2022/day02/index.ts
+++ 2022/day02/index.ts
@@ -5,49 +5,60 @@ const data = await readFile("./2022/day02/input.txt", { encoding: "utf8" });
 const pointsPerShape = (shape) => {
   switch (shape) {
     case "A":
-    case "X":
       return 1;
     case "B":
-    case "Y":
       return 2;
     case "C":
-    case "Z":
       return 3;
     default:
       return 0;
   }
 };
 
-const pointsPerOutcome = (opponentsShape, myShape) => {
+const shapes = ["A", "B", "C"];
+
+const pointsPerOutcome = (outcome) => {
+  switch (outcome) {
+    case "X":
+      return 0;
+    case "Y":
+      return 3;
+    case "Z":
+      return 6;
+  }
+};
+
+// X = loose
+// Y = draw
+// Z = win
+const chooseShape = (opponentsShape, outcome) => {
   // Draw
-  if (
-    (opponentsShape === "A" && myShape === "X") ||
-    (opponentsShape === "B" && myShape === "Y") ||
-    (opponentsShape === "C" && myShape === "Z")
-  )
-    return 3;
-  // Rock
-  if (opponentsShape === "A" && myShape === "Y") return 6; // Win
-  if (opponentsShape === "A" && myShape === "Z") return 0; // Loss
-  // Paper
-  if (opponentsShape === "B" && myShape === "X") return 0;
-  if (opponentsShape === "B" && myShape === "Z") return 6;
-  // Scissors
-  if (opponentsShape === "C" && myShape === "X") return 6;
-  if (opponentsShape === "C" && myShape === "Y") return 0;
+  if (outcome === "Y") return opponentsShape;
+
+  const indexOfShape = shapes.indexOf(opponentsShape);
+
+  // Loss
+  if (outcome === "X") return shapes.at(indexOfShape - 1);
+  // Win
+  if (outcome === "Z") {
+    if (opponentsShape === "C") return shapes[0];
+    return shapes.at(indexOfShape + 1);
+  }
 };
 
 const rounds = data.split("\n");
 
 const pointsPerRounds = [];
 
-for (const [opponent, , me] of rounds) {
-  const perShape = pointsPerShape(me);
-  const perOutcome = pointsPerOutcome(opponent, me);
+for (const [opponent, , outcome] of rounds) {
+  const shape = chooseShape(opponent, outcome);
+
+  const perShape = pointsPerShape(shape);
+  const perOutcome = pointsPerOutcome(outcome);
 
   pointsPerRounds.push(perShape + perOutcome);
 }
 
 const totalScore = pointsPerRounds.reduce((a, b) => a + b, 0);
 
-console.log(totalScore)
+console.log(totalScore);
