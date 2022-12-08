85aed56407929fe91a6dd039a31663ac59dcf512
Janjiran <jiranjan03@gmail.com>
2022-12-08 10:34:00 +0100

    :sparkles: day04 1/2 done

---
+++ 2022/day04/index.ts
@@ -0,0 +1,28 @@
+import { readFile } from "fs/promises";
+
+const rawData = await readFile("./2022/day04/input.txt", { encoding: "utf8" });
+
+const pairs = rawData.split("\n");
+
+let containedRanges = 0;
+
+for (const pair of pairs) {
+  const [section1, section2] = pair.split(",");
+
+  // Define the first range
+  const [start1, end1] = section1.split("-");
+
+  // Define the second range
+  const [start2, end2] = section2.split("-");
+  // Check if the first range is fully contained within the second range
+  // 4-6
+  // 2-8
+  if (Number(start1) >= Number(start2) && Number(end1) <= Number(end2)) {
+    containedRanges++;
+    // Check if the second range is fully contained within the first range
+  } else if (Number(start2) >= Number(start1) && Number(end2) <= Number(end1)) {
+    containedRanges++;
+  }
+}
+
+console.log(containedRanges);
